16b5f084ca1ee41b3a31e5bc893e1ed46030bcec
Casey S Zduniak <32501098+csz5024@users.noreply.github.com>
2019-03-24 23:56:15 -0400

    working iteration
    will go back and clean up comments/code

--- Homework6.py
+++ Homework6.py
@@ -14,10 +14,10 @@ cur.execute("CREATE TABLE emp ( eid int(11) NOT NULL, ename text CHARACTER SET u
 cur.execute("CREATE TABLE dept ( did int(11) NOT NULL, budget int(11) NOT NULL, managerid int(11) NOT NULL, PRIMARY KEY (did)) ENGINE = InnoDB")
 cur.execute("CREATE TABLE works ( eid int(11) NOT NULL, did int(11) NOT NULL, pct_time int(11) NOT NULL, PRIMARY KEY (eid,did)) ENGINE=InnoDB")
 
-line = open("hw6-dataset-sp19.txt", "r")
+
 
 # parses text file and populates MYSQL tables via INSERT
-
+line = open("hw6-dataset-sp19.txt", "r")
 entry = "g"
 shift = 0
 while len(entry) > 0 :
@@ -52,22 +52,67 @@ cur.execute("ALTER TABLE `works` ADD CONSTRAINT `total participation 2` FOREIGN 
 
 connection.commit()
 
+
+
+
 # Question 1
 # search through each did to find an eid that matches the age
-#cur.execute("SELECT did, MAX(age), emp.eid, ename, salary FROM `emp`, `works` WHERE emp.eid=works.eid GROUP BY did")
-cur.execute("SELECT w2.did, e2.eid, e2.ename, e2.age, e2.salary FROM Emp e2 JOIN Works w2 USING (eid) JOIN (SELECT w.did, MAX(e.age) AS Oldest FROM works w INNER JOIN emp e USING (eid) GROUP BY w.did) sq ON w2.did = sq.did and e2.age = sq.Oldest ORDER BY `w2`.`did` ASC")
+# nested join to collect max age from each did, then join to find the eid with that age in that department
+cur.execute("SELECT w2.did, e2.eid, e2.ename, e2.age, e2.salary FROM emp e2 LEFT OUTER JOIN Works w2 USING (eid) JOIN (SELECT w.did, MAX(e.age) AS Oldest FROM works w INNER JOIN emp e USING (eid) GROUP BY w.did) old ON w2.did = old.did and e2.age = old.Oldest ORDER BY `w2`.`did` ASC")
+print("Question #1")
 for (did) in cur:
     print(did)
 
-#do stuff here
+print("")
+
+# Question 2
+# Find the employees who are younger than all managers (assuming managerid=emp.eid)
+cur.execute("SELECT e2.eid, e2.ename, e2.age FROM emp e2 WHERE e2.age < (SELECT MIN(e.age) AS Youngest FROM emp e RIGHT OUTER JOIN dept d ON e.eid=d.managerid) GROUP BY e2.eid")
+print("Question #2")
+for (managerid) in cur:
+    print(managerid)
+
+print("")
+
+# Question 3
+# Find the names, age and salary of each employee who works in the Sales Department but not in Marketing department (assume that sales did=4, and marketing did=6)
+cur.execute("SELECT e.eid, e.ename, e.age, e.salary FROM emp e JOIN (SELECT w.eid FROM works w WHERE w.did=4 AND w.eid NOT IN (SELECT w.eid FROM works w WHERE w.did=6) GROUP BY w.eid) dubya ON e.eid=dubya.eid")
+print("Question #3")
+for (eid) in cur:
+    print(eid)
+
+print("")
+
+# Question 4
+# Find the department with the highest average salary of employees
+cur.execute("SELECT w.did, AVG(t.salary) FROM works w JOIN (SELECT e.eid, e.salary FROM emp e) t ON t.eid=w.eid GROUP BY w.did")
+print("Question #4")
+for (eid) in cur:
+    print(eid)
+
+print("")
+
+# Question 5
+# Find the managers who also work in another department(s) but not as a manager there
+cur.execute("SELECT t2.managerid, t2.did FROM (SELECT DISTINCT md.managerid, w.did FROM works w JOIN (SELECT d.managerid FROM dept d) md ON w.eid=md.managerid) AS t2 LEFT OUTER JOIN (SELECT d2.managerid, d2.did FROM dept d2) AS t3 ON t2.managerid=t3.managerid AND t2.did=t3.did WHERE t3.did IS NULL")
+print("Question #5")
+for (managerid) in cur:
+    print(managerid)
+
+print("")
+
+# Question 6
+# Find the employee ID, name and age of each employee whose salary exceeds the budget of every department that he or she works in
+# Returns null because no employee has a salary higher than ALL departments that they work in. Only eid=21 has a salary higher than ONE of the departments that they work in.
+cur.execute("SELECT e.eid, e.ename, e.age FROM emp e JOIN(SELECT w.eid, w.did, MAX(bd.budget) AS yas FROM works w JOIN (SELECT d.did, d.budget FROM dept d) bd ON w.did=bd.did GROUP BY w.eid) ma ON e.eid=ma.eid WHERE e.salary > yas")
+print("Question #6")
+for (eid) in cur:
+    print(eid)
+
+print("")
+
 connection.commit()
 
-
-
-#cleans up the database afterwards
-#cur.execute("DROP TABLE `works`")
-#cur.execute("DROP TABLE `emp`, `dept`")
-#connection.commit()
 
 cur.close()
 connection.close()
